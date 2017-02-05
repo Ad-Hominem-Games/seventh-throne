604cb5cc42db330af671e64199d54dd6ec31d356
Ryan "Walker" Chilton <rnchltn@gmail.com>
2017-02-05 17:50:09 -0600

    Deletion Update
    Deletion!

--- Assets/Code/player_Component.cs
+++ Assets/Code/player_Component.cs
@@ -11,6 +11,17 @@ public class player_Component : MonoBehaviour
     public Image ComposureBar;
     public bool madeMistake = false;
     public bool isPlayingGame = false;
+    public GameObject ethosSymbol;
+    public GameObject pathosSymbol;
+    public GameObject logosSymbol;
+    public int GameNo = 0;
+    public GameObject BalloonOne;
+    public GameObject BalloonTwo;
+    public GameObject BalloonThree;
+    public GameObject BalloonFour;
+    public GameObject BalloonFive;
+    public GameObject BalloonSix;
+
 
     // Use this for initialization
     void Start()
@@ -37,20 +48,31 @@ public class player_Component : MonoBehaviour
 
         //different argument types (points)
         input = Input.inputString;
+        if (isPlayingGame == true && input == "b")
+        {
+            isPlayingGame = false;
+            EndPoint();
+        }
         if (isPlayingGame == false)
         {
             switch (input)
             {
                 case "z":     //pathos
-                              //start a pathos minigame
+                    pathosSymbol.SetActive(true);
+                    isPlayingGame = true;
+                    //start a pathos minigame
                     break;
 
                 case "x":     //logos
-                              //start a logos minigame
+                    logosSymbol.SetActive(true);
+                    isPlayingGame = true;
+                    //start a logos minigame
                     break;
 
                 case "c":     //ethos
-                              //start an ethos minigame
+                    ethosSymbol.SetActive(true);
+                    isPlayingGame = true;
+                    //start an ethos minigame
                     break;
             }
         }
@@ -68,4 +90,18 @@ public class player_Component : MonoBehaviour
         }
         ComposureBar.fillAmount = 1.0f;
     }
+
+    public void EndPoint()
+    {
+            ethosSymbol.SetActive(false);
+            pathosSymbol.SetActive(false);
+            logosSymbol.SetActive(false);
+        BalloonOne.SetActive(false);
+        BalloonTwo.SetActive(false);
+        BalloonThree.SetActive(false);
+        BalloonFour.SetActive(false);
+        BalloonFive.SetActive(false);
+        BalloonSix.SetActive(false);
+    }
+
 }
