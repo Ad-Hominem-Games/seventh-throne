ffbb6f60d66a18d460ff66a0b89e353dcab2194d
SwammyD <samdoty2018@u.northwestern.edu>
2017-02-20 18:03:15 -0600

    catch up to date

--- Assets/Code/DodgeBlock.cs
+++ Assets/Code/DodgeBlock.cs
@@ -16,11 +16,31 @@ public class DodgeBlock : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.LeftArrow) && this.gameObject.transform.localPosition.x > -45)
         {
-            this.gameObject.transform.position -= new Vector3(5, 0);
+            if (this.Parent.GetComponentInParent<dodgeGame>().player.GetComponent<player2Component>() != null)
+            {
+                this.gameObject.transform.position -= new Vector3(5, 0);
+            }
         }
         if (Input.GetKey(KeyCode.RightArrow) && this.gameObject.transform.localPosition.x < 45)
         {
-            this.gameObject.transform.position += new Vector3(5, 0);
+            if (this.Parent.GetComponentInParent<dodgeGame>().player.GetComponent<player2Component>() != null)
+            {
+                this.gameObject.transform.position += new Vector3(5, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.A) && this.gameObject.transform.localPosition.x > -45)
+        {
+            if (this.Parent.GetComponentInParent<dodgeGame>().player.GetComponent<player_Component>() != null)
+            {
+                this.gameObject.transform.position -= new Vector3(5, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) && this.gameObject.transform.localPosition.x < 45)
+        {
+            if (this.Parent.GetComponentInParent<dodgeGame>().player.GetComponent<player_Component>() != null)
+            {
+                this.gameObject.transform.position += new Vector3(5, 0);
+            }
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
