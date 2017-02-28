85ba77c6d8861373e76b6ea95a6044cc6570e7a7
SwammyD <samdoty2018@u.northwestern.edu>
2017-02-28 16:31:05 -0600

    timer

---
+++ Assets/Code/GameTimer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour {
+
+
+    public GameObject p1;
+    public GameObject p2;
+    public float timeLeft = 60;
+    public int matchNo;
+    public float p1fill;
+    public float p2fill;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    //end state
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+        p1fill = p1.GetComponent<player_Component>().OpinionFill.fillAmount;
+        p2fill = p2.GetComponent<player2Component>().OpinionFill.fillAmount;
+        matchNo++;
+        if (matchNo > 3)
+        {
+            SetOver();
+        }
+        if (p1fill == p2fill)
+        {
+            //...tie game, need to figure that out
+        }
+        else if (p1fill > p2fill)
+        {
+            //trigger topic web, but with p1 in control
+        }
+        else if (p2fill > p1fill)
+        {
+            //trigger topic web, but with p2 in control
+        }
+        timeLeft = 60;
+    }
+    public void SetOver()
+    {
+        if (p1fill > p2fill)
+        {
+            print("Player One Wins!!");
+        }
+        else if (p2fill > p1fill)
+        {
+            print("Player Two Wins!");
+        }
+        //freeze game
+        print("Now Go Home");
+    }
+}
